4521cdb7c91aaca1192176b15050c4e17f1058ce
stephramos <stephaniemnz@gmail.com>
2023-11-18 09:46:49 -0600

    created if statements for other criterias

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var myarray2 = lowerCase.split("");
-console.log (myarray2.length, "length");
+var LcArray = lowerCase.split("");
+const UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var UcArray = UpperCase.split("");
+const numbers = "0123456789";
+var NumArray = numbers.split("");
+const special = "!@#$%^&*()_+=';:?/><.,"
+var SArray = special.split("");
+console.log (LcArray.length, "length");
+console.log (UcArray.length, "length");
+console.log (NumArray.length, "length");
+console.log (SArray.length, "length");
+
 var randomNum = Math.floor(Math.random() * 26);
-console.log(myarray2[Math.floor(Math.random() * 26)] + myarray2[Math.floor(Math.random() * 26)]);
+console.log(LcArray[Math.floor(Math.random() * 26)] + LcArray[Math.floor(Math.random() * 26)]);
 console.log (randomNum);
 
 
 
 // Write password to the #password input
 function generatePassword() {
-  const UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const numbers = "0123456789";
-  const special = "!@#$%^&*()_+=';:?/><.,"
+ 
 
-  const criteria = (prompt("Number of Characters? (8-12)"));
+  const criteria = (prompt("Number of Characters? (8-128)"));
   console.log (criteria);
-  console.log (typeof(criteria));
-  if (criteria > 12 || criteria < 8) {
+  if (criteria > 128 || criteria < 8) {
     alert("Number of characters must be between 8-12");
     return;
   }
-  const criteria1 = confirm("Must include lowercase letter.");
-  const criteria2 = confirm("Must include uppercase letter.");
-  const criteria3 = confirm("Must include at least one number.");
-  const criteria4 = confirm("Must include at least one special character.");
+  const criteria1 = (prompt("Would you like it to include lowercase letters?"));
+  console.log(criteria1);
+if (criteria1 === "yes") { 
+  criteria1 === true;
+}
+  const criteria2 = (prompt)("Would you like it to include uppercase letters?");
+  console.log(criteria2);
+  if (criteria2 === "yes") { 
+    criteria2 === true;
+  }
+  const criteria3 = (prompt("Would you like to include numbers?"));
+  console.log(criteria3);
+  if (criteria3 === "yes") { 
+    criteria3 === true;
+  }
+  const criteria4 = (prompt("Would you like to include special characters?"));
+  console.log(criteria4);
+  if (criteria4 === "yes") { 
+    criteria4 === true;
+  }
 
 if (!(criteria1 || criteria2 || criteria3 || criteria4)) {
   alert("Must contain at least one lowercase, uppercase, number, and special character");
@@ -33,7 +56,7 @@ if (!(criteria1 || criteria2 || criteria3 || criteria4)) {
 }
 var password = ""
  for (let i=0; i<criteria; i++) {
-  password += myarray2[Math.floor(Math.random() * 26)];
+  password += LcArray[Math.floor(Math.random() * 26)];
  } 
  console.log (password);
 const allchars = [lowerCase, UpperCase, numbers, special];
