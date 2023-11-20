2578a22f9de619070b5563e95d1de2495c4cb9ac
stephramos <stephaniemnz@gmail.com>
2023-11-20 17:47:04 -0600

    add criterias if statement with a mutliSelect and edited for loop

--- script.js
+++ script.js
@@ -2,86 +2,69 @@
 var generateBtn = document.querySelector("#generate");
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var LcArray = lowerCase.split("");
+console.log (LcArray.length, "length");
 const UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var UcArray = UpperCase.split("");
+console.log (UcArray.length, "length");
 const numbers = "0123456789";
 var NumArray = numbers.split("");
+console.log (NumArray.length, "length");
 const special = "!@#$%^&*()_+=';:?/><.,"
 var SArray = special.split("");
-console.log (LcArray.length, "length");
-console.log (UcArray.length, "length");
-console.log (NumArray.length, "length");
 console.log (SArray.length, "length");
-
-var randomNum = Math.floor(Math.random() * 26);
-console.log(LcArray[Math.floor(Math.random() * 26)] + LcArray[Math.floor(Math.random() * 26)]);
-console.log (randomNum);
-
-
+var random = Math.floor(Math.random() * 26);
+var multiSelect = [];
 
 // Write password to the #password input
 function generatePassword() {
  
-
-  const criteria = (prompt("Number of Characters? (8-128)"));
+  const criteria = parseInt(prompt("Number of Characters? (8-128)"));
   console.log (criteria);
+  console.log (typeof criteria);
   if (criteria > 128 || criteria < 8) {
     alert("Number of characters must be between 8-12");
     return;
   }
-  const criteria1 = (prompt("Would you like it to include lowercase letters?"));
+  
+  const criteria1 = confirm("Would you like it to include lowercase letters?");
   console.log(criteria1);
-if (criteria1 === "yes") { 
-  criteria1 === true;
-}
-  const criteria2 = (prompt)("Would you like it to include uppercase letters?");
-  console.log(criteria2);
-  if (criteria2 === "yes") { 
-    criteria2 === true;
-  }
-  const criteria3 = (prompt("Would you like to include numbers?"));
-  console.log(criteria3);
-  if (criteria3 === "yes") { 
-    criteria3 === true;
-  }
-  const criteria4 = (prompt("Would you like to include special characters?"));
-  console.log(criteria4);
-  if (criteria4 === "yes") { 
-    criteria4 === true;
-  }
 
+  const criteria2 = confirm("Would you like it to include uppercase letters?");
+  console.log(criteria2);
+ 
+  const criteria3 = confirm("Would you like to include numbers?");
+  console.log(criteria3);
+  
+  const criteria4 = confirm("Would you like to include special characters?");
+  console.log(criteria4);
+ 
 if (!(criteria1 || criteria2 || criteria3 || criteria4)) {
   alert("Must contain at least one lowercase, uppercase, number, and special character");
-  return null;
+  return;
 }
-var password = ""
- for (let i=0; i<criteria; i++) {
-  password += LcArray[Math.floor(Math.random() * 26)];
- } 
+
+if(criteria1) {multiSelect += LcArray;}
+if(criteria2) {multiSelect += UcArray;}
+if(criteria3) {multiSelect += NumArray;}
+if(criteria4) {multiSelect += SArray;}
+
+let password = "";
+for (let i=0; i<criteria; i++) {
+  password += multiSelect[Math.floor(Math.random() * multiSelect.length)];
+}
  console.log (password);
-const allchars = [lowerCase, UpperCase, numbers, special];
-var possibleChars = "";
-
-if (criteria1) possibleChars += lowerCase;
-if (criteria2) possibleChars += UpperCase;
-if (criteria3) possibleChars += numbers;
-if (criteria4) possibleChars += special;
-
 
 return password;
 
 }
 
 function writePassword() {
-  console.log("buttonPress");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   if (password) {
     passwordText.value = password;
   }
-
-
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
