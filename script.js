134ff60e72d2bae4dcdb1c0f698aba1e4d86ec47
stephramos <stephaniemnz@gmail.com>
2023-11-20 22:32:58 -0600

    removed unnecessary console.log's and cleaned added section title

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-// Assignment Code
+//  VARIABLES
 var generateBtn = document.querySelector("#generate");
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var LcArray = lowerCase.split("");
-console.log (LcArray.length, "length");
 const UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var UcArray = UpperCase.split("");
-console.log (UcArray.length, "length");
 const numbers = "0123456789";
 var NumArray = numbers.split("");
-console.log (NumArray.length, "length");
 const special = "!@#$%^&*()+=';:?/><.";
 var SArray = special.split("");
-console.log (SArray.length, "length");
-var random = Math.floor(Math.random() * 26);
-var multiSelect = [];
 
-// Write password to the #password input
+
+// Password Generator
 function generatePassword() {
  
   const criteria = parseInt(prompt("Number of Characters? (8-128)"));
   console.log (criteria);
-  console.log (typeof criteria);
   if (criteria > 128 || criteria < 8) {
     alert("Number of characters must be between 8-12");
     return;
@@ -38,29 +32,37 @@ function generatePassword() {
   const criteria4 = confirm("Would you like to include special characters?");
   console.log(criteria4);
  
+ 
 if (!(criteria1 || criteria2 || criteria3 || criteria4)) {
   alert("Must contain at least one lowercase, uppercase, number, and special character");
   return;
 }
-
+const availableCharacters = [];
+console.log (availableCharacters);
+if (criteria1) {
+  availableCharacters.push(...LcArray);
+}
+if (criteria2) {
+  availableCharacters.push(...UcArray);
+}
+if (criteria3) {  
+  availableCharacters.push(...NumArray);
+}
+if (criteria4) {      
+  availableCharacters.push(...SArray);
+}
 
 let password = "";
 for (let i=0; i<criteria; i++) {
-
-if(criteria1) {password += LcArray[Math.floor(Math.random() * LcArray.length)];}
-if(criteria2) {password += UcArray[Math.floor(Math.random() * UcArray.length)]}
-if(criteria3) {password += NumArray[Math.floor(Math.random() * NumArray.length)];}
-if(criteria4) {password += SArray[Math.floor(Math.random() * SArray.length)];}
-console.log ("password", password);
-
+  password += availableCharacters[Math.floor(Math.random() * availableCharacters.length)];
+console.log (password);
 }
 
 return password;
 }
 
 
-
-
+//Display Password
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -69,6 +71,8 @@ function writePassword() {
     passwordText.value = password;
   }
 }
-// Add event listener to generate button
+
+
+//Event listener
 generateBtn.addEventListener("click", writePassword);
 
